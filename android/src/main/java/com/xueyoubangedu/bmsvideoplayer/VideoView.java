6581d8576860520a26e586247f4edf4b53120cb0
MessiMercy <408708006@qq.com>
2019-07-08 16:23:40 +0800

    Update VideoView.java

--- android/src/main/java/com/xueyoubangedu/bmsvideoplayer/VideoView.java
+++ android/src/main/java/com/xueyoubangedu/bmsvideoplayer/VideoView.java
@@ -1,22 +1,22 @@
 package com.xueyoubangedu.bmsvideoplayer;
 
 import android.content.Context;
-import android.view.View;
 import android.view.LayoutInflater;
-import android.widget.TextView;
-import io.flutter.plugin.common.MethodCall;
-import io.flutter.plugin.common.MethodChannel;
-import static io.flutter.plugin.common.MethodChannel.MethodCallHandler;
-import static io.flutter.plugin.common.MethodChannel.Result;
-import io.flutter.plugin.common.PluginRegistry.Registrar;
-import io.flutter.plugin.platform.PlatformView;
+import android.view.View;
+
 import com.shuyu.gsyvideoplayer.GSYVideoManager;
-import com.shuyu.gsyvideoplayer.utils.OrientationUtils;
-import com.shuyu.gsyvideoplayer.video.StandardGSYVideoPlayer;
-import com.shuyu.gsyvideoplayer.GSYVideoManager;
+import com.shuyu.gsyvideoplayer.builder.GSYVideoOptionBuilder;
 import com.shuyu.gsyvideoplayer.listener.GSYSampleCallBack;
 import com.shuyu.gsyvideoplayer.listener.LockClickListener;
-import com.shuyu.gsyvideoplayer.builder.GSYVideoOptionBuilder;
+import com.shuyu.gsyvideoplayer.utils.OrientationUtils;
+import com.shuyu.gsyvideoplayer.video.StandardGSYVideoPlayer;
+
+import io.flutter.plugin.common.MethodCall;
+import io.flutter.plugin.common.MethodChannel;
+import io.flutter.plugin.common.PluginRegistry.Registrar;
+import io.flutter.plugin.platform.PlatformView;
+
+import static io.flutter.plugin.common.MethodChannel.MethodCallHandler;
 
 public class VideoView implements PlatformView, MethodCallHandler {
     private StandardGSYVideoPlayer bmsVideo;
@@ -44,6 +44,9 @@ public class VideoView implements PlatformView, MethodCallHandler {
                 String url = methodCall.arguments.toString();
                 getBmsVideo(url);
                 break;
+            case "dispose":
+                GSYVideoManager.releaseAllVideos();
+                break;
             default:
                 result.notImplemented();
         }
@@ -51,13 +54,14 @@ public class VideoView implements PlatformView, MethodCallHandler {
     }
 
     @Override
-    public void dispose() {}
+    public void dispose() {
+    }
 
     private void getBmsVideo(String url) {
         // 初始化
         //设置旋转
         orientationUtils = new OrientationUtils(registrar.activity(), bmsVideo);
-        
+
         //是否可以滑动调整
         bmsVideo.setIsTouchWiget(true);
         //设置返回按键
@@ -68,40 +72,40 @@ public class VideoView implements PlatformView, MethodCallHandler {
 
         gsyVideoOption = new GSYVideoOptionBuilder();
         gsyVideoOption.setIsTouchWiget(true)
-            .setRotateViewAuto(false)
-            .setLockLand(false)
-            .setAutoFullWithSize(true)
-            .setShowFullAnimation(false)
-            .setNeedLockFull(true)
-            .setUrl(url)
-            .setCacheWithPlay(true)
-            .setVideoAllCallBack(new GSYSampleCallBack() {
-                @Override
-                public void onPrepared(String url, Object... objects) {
-                    super.onPrepared(url, objects);
-                    //开始播放了才能旋转和全屏
-                    orientationUtils.setEnable(true);
-                    // isPlay = true;
-                }
-
-                @Override
-                public void onQuitFullscreen(String url, Object... objects) {
-                    super.onQuitFullscreen(url, objects);
-                    if (orientationUtils != null) {
-                        orientationUtils.backToProtVideo();
+                .setRotateViewAuto(false)
+                .setLockLand(false)
+                .setAutoFullWithSize(true)
+                .setShowFullAnimation(false)
+                .setNeedLockFull(true)
+                .setUrl(url)
+                .setCacheWithPlay(true)
+                .setVideoAllCallBack(new GSYSampleCallBack() {
+                    @Override
+                    public void onPrepared(String url, Object... objects) {
+                        super.onPrepared(url, objects);
+                        //开始播放了才能旋转和全屏
+                        orientationUtils.setEnable(true);
+                        // isPlay = true;
                     }
-                }
-            }).setLockClickListener(new LockClickListener() {
-                @Override
-                public void onClick(View view, boolean lock) {
-                    if (orientationUtils != null) {
-                        //配合下方的onConfigurationChanged
-                        orientationUtils.setEnable(!lock);
-                    }
-                }
-            }).build(bmsVideo);
 
-            bmsVideo.getFullscreenButton().setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onQuitFullscreen(String url, Object... objects) {
+                        super.onQuitFullscreen(url, objects);
+                        if (orientationUtils != null) {
+                            orientationUtils.backToProtVideo();
+                        }
+                    }
+                }).setLockClickListener(new LockClickListener() {
+            @Override
+            public void onClick(View view, boolean lock) {
+                if (orientationUtils != null) {
+                    //配合下方的onConfigurationChanged
+                    orientationUtils.setEnable(!lock);
+                }
+            }
+        }).build(bmsVideo);
+
+        bmsVideo.getFullscreenButton().setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 orientationUtils.resolveByClick();
